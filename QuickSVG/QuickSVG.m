c15e8b4e0290e5c6d8bad3e6773d3a43dc5fda74
Ben Weaver <ben@orangesoda.net>
2012-10-12 12:58:33 -0400

    transformForSVGMatrix moved.

--- QuickSVG/QuickSVG.m
+++ QuickSVG/QuickSVG.m
@@ -161,7 +161,7 @@
 
 	QuickSVGInstance *instance = [[QuickSVGInstance alloc] initWithFrame:frame];
 	[instance.attributes addEntriesFromDictionary:attributes];
-	CGAffineTransform viewTransform = [self transformForSVGMatrix:attributes];
+	CGAffineTransform viewTransform = [instance transformForSVGMatrix:attributes];
 	instance.transform = viewTransform;
 	instance.quickSVG = self;
 	
